6b786fa8ee4b6eccde28fd922ee0be0978df3511
Bodnar, Hanna <htuple3@gatech.edu>
2024-11-21 21:58:23 -0500

    Add js folder for js scripts and functions.

--- libs/districtSelection.js
+++
@@ -1,7 +0,0 @@
-let selectedDistrict = null;
-
-// Function to highlight the selected district
-function highlightDistrict(districtElement) {
-    d3.selectAll(".district").style("fill", "#69b3a2");
-    d3.select(`[data=id='${districtElement.properties.DISTRICT}']`).style("fill", "#2c7fb8");
-}
